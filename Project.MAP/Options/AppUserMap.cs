3b3a8d4f620895c7d2f8d949786feef098080793
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2020-01-26 13:57:37 +0300

    DAL katmanı yazıldı

---
+++ Project.MAP/Options/AppUserMap.cs
@@ -0,0 +1,20 @@
+﻿using Project.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class AppUserMap:BaseMap<AppUser>
+    {
+        //Turgut:Map'de Türkce'lestirme ayarları
+        public AppUserMap()
+        {
+            ToTable("Kullanıcılar");
+
+            HasOptional(x => x.AppUserDetail).WithRequired(x => x.AppUser);
+        }
+    }
+}
