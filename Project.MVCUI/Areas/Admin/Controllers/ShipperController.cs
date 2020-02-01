2108698f560ccea1676b99fd63b89c16b4b4dcd7
Ecenaz Çeşme <ecenazcesme@windowslive.com>
2020-02-01 13:41:21 +0300

    ShipperController eklendi.

---
+++ Project.MVCUI/Areas/Admin/Controllers/ShipperController.cs
@@ -0,0 +1,57 @@
+﻿using Project.BLL.DesignPatterns.RepositoryPattern.ConcRep;
+using Project.MODEL.Entities;
+using Project.MVCUI.AuthenticationClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Areas.Admin.Controllers
+{
+    [AdminAuth]
+    public class ShipperController : Controller
+    {
+        ShipperRepository sRep;
+        public ShipperController()
+        {
+            sRep = new ShipperRepository();
+        }
+        // GET: Admin/Shipper
+        public ActionResult ShipperList()
+        {
+            return View(sRep.GetAll());
+        }
+
+        public ActionResult AddShipper()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddShipper( Shipper item)
+        {
+            sRep.Add(item);
+            return RedirectToAction("ShipperList");
+        }
+
+        public ActionResult DeleteShipper(int id)
+        {
+            sRep.Delete(sRep.Find(id));
+            return RedirectToAction("ShipperList");
+        }
+
+        public ActionResult UpdateShipper()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateShipper(Shipper item)
+        {
+            sRep.Update(item);
+            return RedirectToAction("ShipperList");
+        }
+
+    }
+}
